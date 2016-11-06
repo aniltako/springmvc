e75be9a7699a3e7717fe146e2507c4a10d689cf6
aniltako <anil.tako@fusemachines.com>
2016-11-06 22:45:47 +0545

    add project

---
+++ src/main/java/com/spring/mvc/config/AppConfig.java
@@ -0,0 +1,20 @@
+package com.spring.mvc.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "com.spring.mvc")
+public class AppConfig {
+	
+	private static final Logger logger = LoggerFactory.getLogger(AppConfig.class);
+	
+	public void FirstMethod(){
+		logger.info("Adding method");
+	}
+	
+}
---
+++ src/main/java/com/spring/mvc/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.spring.mvc.controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+ 
+@RestController
+public class HelloController {
+ 
+    @RequestMapping(method = RequestMethod.GET)
+    public String sayHello(ModelMap model) {
+        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        return "welcome";
+    }
+ 
+    @RequestMapping(value = "/helloagain", method = RequestMethod.GET)
+    public String sayHelloAgain() {
+        return "welcome";
+    }
+ 
+}
