c01678a7dd647cdefc4b86fe042d69b62beb9179
aniltako <anil.tako@fusemachines.com>
2016-11-08 17:07:46 +0545

    Add Hibernate Configuration

--- src/main/java/com/spring/mvc/config/AppConfig.java
+++ src/main/java/com/spring/mvc/config/AppConfig.java
@@ -2,16 +2,32 @@ package com.spring.mvc.config;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+
 @Configuration
+@Import({MysqlDbConfig.class})
 @EnableWebMvc
 @ComponentScan(basePackages = "com.spring.mvc")
 public class AppConfig {
 	
 	private static final Logger logger = LoggerFactory.getLogger(AppConfig.class);
+	
+	
+	 @Bean
+	 public PropertyPlaceholderConfigurer getPropertyPlaceholderConfigurer()
+	    {
+	        PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
+	        ppc.setLocation(new ClassPathResource("config/config.properties"));
+	        ppc.setIgnoreUnresolvablePlaceholders(true);
+	        return ppc;
+	    }
 	
 	public void FirstMethod(){
 		logger.info("Adding method");
